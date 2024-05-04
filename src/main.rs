1c5cfba63fcd274d70278d62398fc80a9063e997
Felipe Pitol Puhl <felpofo@proton.me>
2024-05-04 13:50:05 -0300

    refactor colorizer

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,68 @@ use std::io::{BufReader, Read};
 use std::env;
 use std::process;
 
+enum AsciiType {
+    Null,
+    Escape,
+    Whitespace,
+    Character,
+    Number,
+    Punctuation,
+    Exotic
+}
+
 const BUFFER_SIZE: usize = 16;
+
+// this table is nuts
+const ASCII_TYPES: [AsciiType; 128] = {
+    use AsciiType::*;
+
+    [
+        Null, // null
+        Exotic, Exotic, // start of heading/text
+        Exotic, Exotic, // end of text/transmission
+        Exotic, Exotic, Exotic, // enquiry/acknowledge/bell
+        Escape, Whitespace, // backspace/TAB
+        Escape, Exotic, // LF/vertical tab
+        Exotic, Escape, // FF/CR
+        Exotic, Exotic, // shift out/in
+        Exotic, // data link escape
+        Exotic, Exotic, Exotic, Exotic, // device control 1/2/3/4
+        Exotic, Exotic, Exotic, // NAK/SYN/ETB
+        Escape, // cancel
+        Exotic, Exotic, // end of medium/substitute
+        Escape,
+        Exotic, Exotic, Exotic, Exotic, // file/group/record/unit separator
+        Whitespace,
+        Punctuation, Punctuation, Punctuation, // ! " #
+        Punctuation, Punctuation, Punctuation, // $ % &
+        Punctuation, Punctuation, Punctuation, // ' ( )
+        Punctuation, Punctuation, Punctuation, // * + ,
+        Punctuation, Punctuation, Punctuation, // - . /
+        Number, Number, Number, // 0 1 2
+        Number, Number, Number, // 3 4 5
+        Number, Number, Number, // 6 7 8
+        Number, // 9
+        Punctuation, Punctuation, Punctuation, // : ; <
+        Punctuation, Punctuation, Punctuation, // = > ?
+        Punctuation, // @
+        Character, Character, Character, Character, Character, Character, // A B C D E F
+        Character, Character, Character, Character, Character, Character, // G H I J K L
+        Character, Character, Character, Character, Character, Character, // M N O P Q R
+        Character, Character, Character, Character, Character, Character, // S T U V W X
+        Character, Character, // Y Z
+        Punctuation, Punctuation, Punctuation, // [ \ ]
+        Punctuation, Punctuation, Punctuation, // ^ _ `
+        Character, Character, Character, Character, Character, Character, // a b c d e f
+        Character, Character, Character, Character, Character, Character, // g h i j k l
+        Character, Character, Character, Character, Character, Character, // m n o p q r
+        Character, Character, Character, Character, Character, Character, // s t u v w x
+        Character, Character, // y z
+        Punctuation, Punctuation, Punctuation, // { | }
+        Punctuation, // ~
+        Escape, // delete
+    ]
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = env::args().skip(1).collect::<Vec<_>>();
@@ -68,18 +129,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 fn paint_bytes(bytes: &[u8]) -> Vec<(u8, (char, &'static str))> {
     bytes.iter().map(|&byte| {
-        let pretty = if byte as char == ' ' {
-            (byte as char, "0")
-        } else if byte.is_ascii_digit() {
-            (byte as char, "33")
-        } else if byte.is_ascii_alphabetic() {
-            (byte as char, "32")
-        } else if byte.is_ascii_punctuation() {
-            (byte as char, "36")
-        } else if byte.is_ascii_control() {
-            ('.', "31")
-        } else {
-            ('.', "0")
+        use AsciiType::*;
+        let pretty = 'pretty: {
+            if byte >= 128 {
+                if byte == !0 {
+                    break 'pretty ('.', "34");
+                }
+
+                break 'pretty ('.', "0");
+            }
+
+            match ASCII_TYPES[byte as usize] {
+                Null =>        ('.', "30"),
+                Whitespace =>  (' ', "0"),
+                Character =>   (byte as char, "32"),
+                Number =>      (byte as char, "33"),
+                Punctuation => (byte as char, "36"),
+                Exotic =>      ('.', "35"),
+                Escape =>      ('.', "31")
+            }
         };
 
         (byte, pretty)
